efcd8ee83f7ca596bd8621de428791064e026005
saksijas <mustafa@linumlabs.com>
2022-08-08 00:22:59 +0200

    removed comment

--- src/compound_plugin.h
+++ src/compound_plugin.h
@@ -81,8 +81,6 @@ typedef struct context_t {
 
 extern const uint8_t *const COMPOUND_SELECTORS[NUM_SELECTORS];
 
-Piece of code that will check that the above structure is not bigger than 5 * 32. Do not remove
-this check.
 _Static_assert(sizeof(context_t) <= 5 * 32, "Structure of parameters too big.");
 
 void handle_provide_parameter(void *parameters);
