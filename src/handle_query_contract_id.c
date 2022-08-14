519f6f5db41de4a84f394589ca9f82f4f681868f
saksijas <mustafa@linumlabs.com>
2022-08-14 19:56:33 +0200

    removed title

--- src/handle_query_contract_id.c
+++ src/handle_query_contract_id.c
@@ -8,8 +8,7 @@ void handle_query_contract_id(void *parameters) {
     // msg->version will be the lower sentence displayed on the screen.
 
     // For the first screen, display the plugin name.
-    memset(msg->title, 0, msg->titleLength);
-    memset(msg->msg, 0, msg->msgLength);
+    memset(msg->version, 0, msg->versionLength);
     msg->result = ETH_PLUGIN_RESULT_OK;
 
     switch (context->selectorIndex) {
