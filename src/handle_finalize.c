ad18817563443a353221fde86c10baf1ca3832f4
saksijas <mustafa@linumlabs.com>
2023-05-28 21:50:11 +0200

    feat: adding new version for compound

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -6,46 +6,16 @@ void handle_finalize(void *parameters) {
 
     msg->uiType = ETH_UI_TYPE_GENERIC;
 
-    // Setting number of screens based on function
-    switch (context->selectorIndex) {
-        case COMPOUND_REDEEM_UNDERLYING:
-            msg->numScreens = 0;
-            break;
-        case COMPOUND_REDEEM:
-            msg->numScreens = 0;
-            break;
-        case COMPOUND_MINT:
-            msg->numScreens = 0;
-            break;
-        case COMPOUND_BORROW:
-            msg->numScreens = 0;
-            break;
-        case COMPOUND_REPAY_BORROW:
-            msg->numScreens = 0;
-            break;
-        case COMPOUND_REPAY_BORROW_ON_BEHALF:
-            msg->numScreens = 1;
-            break;
-        case COMPOUND_TRANSFER:
-            msg->numScreens = 1;
-            break;
-        case COMPOUND_LIQUIDATE_BORROW:
-            msg->numScreens = 2;
-            break;
-        case COMPOUND_MANUAL_VOTE:
-            msg->numScreens = 1;
-            break;
-        case COMPOUND_VOTE_DELEGATE:
-            msg->numScreens = 0;
-            break;
-        // Keep this
-        default:
-            msg->numScreens = 0;
-    }
-    if (memcmp(msg->address, context->dest, ADDRESS_LENGTH) != 0) {
+    // EDIT THIS: Set the total number of screen you will need.
+    msg->numScreens = 2;
+    // amount + asset
+    if (memcmp(msg->address, context->beneficiary, ADDRESS_LENGTH) != 0) {
         msg->numScreens += 1;
     }
-    // msg->tokenLookup1 = context->collateral;
+
+    // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
+    // token addresses you will info for (such as decimals, ticker...).
+    msg->tokenLookup1 = context->asset;
 
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
--- tests/src/generate_plugin_config.js
+++ tests/src/generate_plugin_config.js
@@ -2,7 +2,7 @@
 // You will also need to create a `b2c.json` file that will hold the methodIDs and location of
 // the erc20 tokens that should get displayed.
 // EDIT THIS: replace with the name of your plugin (lowercase)
-const pluginFolder = "compound";
+const pluginFolder = "boilerplate";
 
 function serialize_data(pluginName, contractAddress, selector) {
 	const len = Buffer.from([pluginName.length]);
@@ -21,10 +21,11 @@ function assert(condition, message) {
 }
 
 // Function to generate the plugin configuration.
-function generate_plugin_config() {
+function generate_plugin_config(network="ethereum") {
 	
 	var fs = require('fs');
-	var files = fs.readdirSync(`${pluginFolder}/abis/`);
+	var files = fs.readdirSync(`networks/${network}/${pluginFolder}/abis/`);
+
 	
 	// `contracts_to_abis` holds a maping of contract addresses to abis
 	let contracts_to_abis = {};
@@ -34,13 +35,14 @@ function generate_plugin_config() {
 		// Strip ".json" suffix
 		let contractAddress = abiFileName.slice(0, abiFileName.length - ".json".length);
 		// Load abi
-		let abi = require(`../${pluginFolder}/abis/${abiFileName}`);
+		let abi = require(`../networks/${network}/${pluginFolder}/abis/${abiFileName}`);
 		// Add it to contracts
 		contracts_to_abis[contractAddress] = abi;
 	}
 	
 	// Load the b2c.json file
-	const b2c = require(`../compound/abis/b2c.json`);
+	const b2c = require(`../networks/${network}/${pluginFolder}/b2c.json`);
+
 	
 	let res = {};
 	
