7a43eefe499827bbbdd26e7444dccefda4806a60
saksijas <mustafa@linumlabs.com>
2023-05-28 22:06:13 +0200

    fix: changing name of renamed interface

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -4,7 +4,7 @@
 // `msg->token1` and `msg->token2`. If those pointers are `NULL`, this means the ethereum app didn't
 // find any info regarding the requested tokens!
 void handle_provide_token(void *parameters) {
-    ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
+    ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
     context_t *context = (context_t *) msg->pluginContext;
 
     if (msg->item1) {
